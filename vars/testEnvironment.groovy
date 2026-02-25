08ae08afb110099c173618020258ad8a9a0052f7
KevinLagaza <92716272+KevinLagaza@users.noreply.github.com>
2026-02-25 14:36:46 +0100

    Create testEnvironment.groovy

---
+++ vars/testEnvironment.groovy
@@ -0,0 +1,17 @@
+def testEnvironment(Map config) {
+    echo "========== TESTING ENVIRONMENT =========="
+    
+    sshagent(credentials: [config.sshCredentialId]) {
+        sh """
+            ssh -o StrictHostKeyChecking=no ${SSH_USER}@${config.host} '
+                echo "=== Checking containers ===" &&
+                docker ps | grep -E "${APP_NAME}|${DB_CONTAINER_NAME}" &&
+                
+                echo "=== Cleanup ===" &&
+                rm -rf /tmp/database &&
+                
+                echo "=== Test completed ==="
+            '
+        """
+    }
+}
